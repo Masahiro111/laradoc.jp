26d518d70773a46c26533db3b38d53532245e062
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-07-16 19:41:07 +0900

    fix pull action code

--- public/vS4wcnTC.php
+++ public/vS4wcnTC.php
@@ -4,11 +4,13 @@
 echo "<p>webhook start</p>";
 exec('cd /home/masahiro111/laradoc/', $op);
 print_r($op);
-exec('git pull');
+exec('git fetch origin/main');
+exec('git reset --hard origin/main');
 
 // document
 exec('cd /home/masahiro111/laradoc/resources/docs/', $op);
 print_r($op);
-exec('git pull');
+exec('git fetch origin/main');
+exec('git reset --hard origin/main');
 
 echo "<p>webhook finish</p>";
