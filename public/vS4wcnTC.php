3d2e4f97277214d5dc5b766075c37f2c0aeaad70
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-07-18 04:15:22 +0900

    fix

--- public/vS4wcnTC.php
+++ public/vS4wcnTC.php
@@ -18,19 +18,19 @@ print_r($result);
 
 
 // document
-exec('cd /home/masahiro111/laradoc/resources/docs/', $output, $result);
+exec('cd /home/masahiro111/laradoc/resources/docs/ && git fetch origin main && git reset --hard origin/main', $output, $result);
 var_dump($output);
 print_r($result);
 
 // print_r($result);
 // exec('git pull', $output, $result);
 // print_r($result);
-exec('git fetch origin main', $output, $result);
-var_dump($output);
-print_r($result);
+// exec('git fetch origin main', $output, $result);
+// var_dump($output);
+// print_r($result);
 
-exec('git reset --hard origin/main', $output, $result);
-var_dump($output);
-print_r($result);
+// exec('git reset --hard origin/main', $output, $result);
+// var_dump($output);
+// print_r($result);
 
 echo "<p>webhook finish</p>";
