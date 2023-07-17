6361ed4c5480cc49d8cb3b83866b111d96057d38
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-07-18 04:04:39 +0900

    wip

--- public/vS4wcnTC.php
+++ public/vS4wcnTC.php
@@ -3,18 +3,27 @@
 // laradoc
 echo "<p>webhook start</p>";
 exec('cd /home/masahiro111/laradoc/', $output, $result);
-// print_r($result);
+print_r($output . $result);
 // exec('git pull', $output, $result);
 // print_r($result);
-exec('git fetch origin main');
-exec('git reset --hard origin/main');
+exec('git fetch origin main', $output, $result);
+print_r($output . $result);
+
+exec('git reset --hard origin/main', $output, $result);
+print_r($output . $result);
+
 
 // document
 exec('cd /home/masahiro111/laradoc/resources/docs/', $output, $result);
+print_r($output . $result);
+
 // print_r($result);
 // exec('git pull', $output, $result);
 // print_r($result);
-exec('git fetch origin main');
-exec('git reset --hard origin/main');
+exec('git fetch origin main', $output, $result);
+print_r($output . $result);
+
+exec('git reset --hard origin/main', $output, $result);
+print_r($output . $result);
 
 echo "<p>webhook finish</p>";
