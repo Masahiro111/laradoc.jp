c6bd5fb485afe6f1d82fd343b09b03f64569da6b
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-07-18 04:06:46 +0900

    wip

--- public/vS4wcnTC.php
+++ public/vS4wcnTC.php
@@ -3,27 +3,34 @@
 // laradoc
 echo "<p>webhook start</p>";
 exec('cd /home/masahiro111/laradoc/', $output, $result);
-print_r($output . $result);
+var_dump($output);
+print_r($result);
+
 // exec('git pull', $output, $result);
 // print_r($result);
 exec('git fetch origin main', $output, $result);
-print_r($output . $result);
+var_dump($output);
+print_r($result);
 
 exec('git reset --hard origin/main', $output, $result);
-print_r($output . $result);
+var_dump($output);
+print_r($result);
 
 
 // document
 exec('cd /home/masahiro111/laradoc/resources/docs/', $output, $result);
-print_r($output . $result);
+var_dump($output);
+print_r($result);
 
 // print_r($result);
 // exec('git pull', $output, $result);
 // print_r($result);
 exec('git fetch origin main', $output, $result);
-print_r($output . $result);
+var_dump($output);
+print_r($result);
 
 exec('git reset --hard origin/main', $output, $result);
-print_r($output . $result);
+var_dump($output);
+print_r($result);
 
 echo "<p>webhook finish</p>";
